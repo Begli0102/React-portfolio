29c47c42b4c6c6323b96a790ffe0ddbd72b742e2
Begli Kakabalov <begli.kaka@mail.ru>
2025-06-15 11:58:24 +0200

    changed the introductory sentence.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react'
 // import ReactTypingEffect from 'react-typing-effect';
-import "./Home.css";
+import './Home.css'
 
 const Home = () => {
   return (
-    <section id="intro">
-      <div className="intro container">
+    <section id='intro'>
+      <div className='intro container'>
         <div>
           <h1>
             Hi there...<span></span>
           </h1>
           <h1>
-            I am a <span></span>
+            I am <span></span>
           </h1>
           <h1>
-            {" "}
-            Frontend Developer<span></span>
+            Begli Kakabalov. Here, you can take a look at my projects
+            <span></span>
           </h1>
           {/* <a href="https://begli0102.github.io/React-portfolio/about" tape='button' className="btn">Learn more</a> */}
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
